57c312608903b1edbcc426725894366ef8148fe9
assajjade <abdillah.assajjad@ui.ac.id>
2024-06-16 02:02:34 +0700

    Editing backend url

--- src/components/Axios.js
+++ src/components/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = "http://127.0.0.1:8000/"
+const baseUrl = "https://thefloatingschoolbe.vercel.app/"
 const AxiosInstance = axios.create({
     baseURL: baseUrl, 
     timeout: 5000, 
